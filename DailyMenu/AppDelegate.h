30352c4bc858340fc524b2e5fc831fff9199069a
菲旸 <xfy@>
2016-09-22 22:04:41 +0800

    Initial Commit

---
+++ DailyMenu/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DailyMenu
+//
+//  Created by 菲旸 on 16/9/22.
+//  Copyright © 2016年 xfy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DailyMenu/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DailyMenu
+//
+//  Created by 菲旸 on 16/9/22.
+//  Copyright © 2016年 xfy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
